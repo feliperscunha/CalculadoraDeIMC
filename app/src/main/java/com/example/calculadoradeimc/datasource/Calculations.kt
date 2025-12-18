05777d45e19b87164d6d68ceb8c57bfa1c51bc9b
feliperscunha <ruascunhafelipe@gmail.com>
2025-12-18 18:05:22 -0300

    Melhora do calculo do TMB

--- app/src/main/java/com/example/calculadoradeimc/datasource/Calculations.kt
+++ app/src/main/java/com/example/calculadoradeimc/datasource/Calculations.kt
@@ -21,9 +21,9 @@ object Calculations {
 
     fun calculateTMB(weight: Double, height: Double, age: Int, gender: Int): Double {
         return if (gender == 0) {
-            88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+            5 + (10 * weight) + (6.25 * height) - (5 * age)
         } else {
-            447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+            (10 * weight) + (6.25 * height) - (5 * age) - 161
         }
     }
 
